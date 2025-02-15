ea60c009b6a38ee221accebe97ff462ea3f9808b
Zaid Patel <patelzaid721@gmail.com>
2025-02-15 10:53:41 +0530

    added dynamic database

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const mongoUrl = process.env.MONGO_URI ? process.env.MONGO_URI : 'mongodb://localhost:27017/studentPortal';
+
 async function connectToDb(){
     try {
-        await mongoose.connect('mongodb://localhost:27017/studentPortal')
+        await mongoose.connect(mongoUrl)
         console.log('connected to database')
     } catch{
         console.log('unable to connect to database')
