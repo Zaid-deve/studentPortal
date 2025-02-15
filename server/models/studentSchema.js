38bdfe506e32b45c8180addfaf5f9cf72bb99fbc
Zaid Patel <patelzaid721@gmail.com>
2025-02-15 12:20:21 +0530

    fixed schema error

--- server/models/studentSchema.js
+++ server/models/studentSchema.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        match: [/^\S+@\S+\.\S+$/, "Invalid email format"]
+    },
     password: { type: String, required: true },
-    phone: { type: String, required:true, unique:true },
+    phone: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        match: [/^\d{10}$/, "Phone number must be exactly 10 digits"] 
+    },
     appliedApplications: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Application' }]
 }, { timestamps: true });
 
